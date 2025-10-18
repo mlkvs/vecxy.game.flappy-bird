3f53c6b48a9efe8bec34671a2a8d419d52f8471e
Dmitry Melkov <melkov.dev@yandex.ru>
2025-10-18 13:24:37 +0300

    [Project] Create base project from vecxy engine

---
+++ sources/FlappyBird.Game/Boot.cs
@@ -0,0 +1,14 @@
+﻿using FlappyBird.Meta;
+using Vecxy.Engine;
+
+namespace FlappyBird.Game;
+
+public class Boot : Component
+{
+    public override void OnAwake()
+    {
+        var menu = new Menu();
+        
+        menu.Open();
+    }
+}
---
+++ sources/FlappyBird.Meta/Menu.cs
@@ -0,0 +1,14 @@
+﻿namespace FlappyBird.Meta;
+
+public class Menu
+{
+    public void Open()
+    {
+        
+    }
+
+    public void Close()
+    {
+        
+    }
+}
