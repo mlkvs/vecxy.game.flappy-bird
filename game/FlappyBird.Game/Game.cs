6897d7a2f0e8aaf7b524d107f78f931ad10d14d2
Dmitry Melkov <melkov.dev@yandex.ru>
2025-10-23 05:03:59 +0300

    [Project] [Settings] Update structure

---
+++ game/FlappyBird.Game/Game.cs
@@ -0,0 +1,22 @@
+﻿using Vecxy.Engine;
+using Zenject;
+
+namespace FlappyBird.Game;
+
+public class Game : IGame
+{
+    public void InstallBindings(DiContainer container)
+    {
+        
+    }
+
+    public void Initialize()
+    {
+        
+    }
+
+    public void Update(float deltaTime)
+    {
+        
+    }
+}
