9eeb6f4481b49995bb3cf3083a29f61216a6bb40
Dmitry Melkov <melkov.dev@yandex.ru>
2025-10-23 07:34:13 +0300

    [Project] [Settings] Update debug mode

--- sources/game/FlappyBird.Game/Game.cs
+++ sources/game/FlappyBird.Game/Game.cs
@@ -1,18 +1,25 @@
-﻿using Vecxy.Engine;
+﻿using System;
+using Vecxy.Engine;
 using Zenject;
 
 namespace FlappyBird.Game;
 
 public class Game : IGame
 {
+    public Game()
+    {
+        Console.WriteLine("test2");
+    }
+    
     public void InstallBindings(DiContainer container)
     {
-        
+        System.Diagnostics.Debugger.Break();
+        Console.WriteLine("testasas");
     }
 
     public void Initialize()
     {
-        
+        Console.WriteLine("test");
     }
 
     public void Update(float deltaTime)
