143955d35bef0fc7df802d2c3842465542cb1e36
Dmitry Melkov <melkov.dev@yandex.ru>
2026-01-08 14:00:50 +0300

    [Project] Update structure project folder

---
+++ Code/FlappyBird.Game/Game.cs
@@ -0,0 +1,22 @@
+﻿using Vecxy;
+using Vecxy.Diagnostics;
+
+namespace FlappyBird.Game;
+
+public class Game : Vecxy.Engine.Game
+{
+    public Game()
+    {
+        Logger.Info("Game.Ctor");
+    }
+
+    public override void Initialize()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
