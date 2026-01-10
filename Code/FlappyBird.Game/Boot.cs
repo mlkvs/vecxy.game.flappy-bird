6ff3e5e93c56d04a6a157d7b9c371213c2495ae2
Dmitry Melkov <melkov.dev@yandex.ru>
2026-01-10 18:51:52 +0300

    - update structure porject and split release and debug builds

--- Code/FlappyBird.Game/Boot.cs
+++ Code/FlappyBird.Game/Boot.cs
@@ -1,13 +1,18 @@
-﻿using Vecxy;
+﻿using Test;
+using Vecxy;
 using Vecxy.Diagnostics;
+using Vecxy.Engine;
 
 namespace FlappyBird.Game;
 
-public class Game : Vecxy.Engine.Game
+public class Boot : Vecxy.Engine.Game
 {
-    public Game()
+    public Boot(GameInfo info) : base(info)
     {
         Logger.Info("Game.Ctor");
+
+        var t = new Class1();
+        t.Test();
     }
 
     public override void Initialize()
---
+++ Code/Test/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Test;
+
+public class Class1
+{
+    public void Test()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
