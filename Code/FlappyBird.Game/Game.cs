02dd8cc856accf575cd45842e99794b2c3c4e278
Dmitry Melkov <melkov.dev@yandex.ru>
2026-02-02 02:47:26 +0700

    [Project] Update structure project folder and system build

--- Code/FlappyBird.Game/Game.cs
+++ Code/FlappyBird.Game/Game.cs
@@ -1,25 +1,20 @@
-﻿using Test;
+using System.Globalization;
 using Vecxy.Diagnostics;
 using Vecxy.Engine;
 
 namespace FlappyBird.Game;
 
-public class Game : Application
+public class Game : AppLayer
 {
-    public Game()
-    {
-        Logger.Info("Game.Ctor");
-
-        var t = new Class1();
-        t.Test();
-    }
-
     public override void OnInitialize()
     {
-       
+        Logger.Info(App.Info.Name);
     }
 
-    public override void OnTick()
+    public override void OnTick(float dt)
     {
+        Logger.Info("dt: " + dt.ToString(CultureInfo.InvariantCulture));
+        
+        Logger.Error("Test");
     }
 }
